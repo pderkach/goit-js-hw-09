9e9e45ccb01e10ad047675ee10dd32b4ce584ff9
pderkach <105808284+pderkach@users.noreply.github.com>
2023-08-27 16:22:02 +0000

    Deploying to gh-pages from @ pderkach/goit-js-hw-09@7117de48685d1d223c619eab840920937a5610e1 🚀

---
+++ 01-color-switcher.56e41c14.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),e=null;t.addEventListener("click",(function(){null===e&&(t.disabled=!0,e=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3))})),n.addEventListener("click",(function(){clearInterval(e),e=null,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.56e41c14.js.map
