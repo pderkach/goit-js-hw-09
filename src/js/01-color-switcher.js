7117de48685d1d223c619eab840920937a5610e1
Paul Derkach <mr.derkach.55@gmail.com>
2023-08-27 19:21:30 +0300

    finished all tasks

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+let intervalId = null;
+
+startButton.addEventListener('click', () => {
+  if (intervalId === null) {
+    startButton.disabled = true;
+    intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(intervalId);
+  intervalId = null;
+  startButton.disabled = false;
+});
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,78 @@
+// import Notiflix from 'notiflix';
+
+// function createPromise(position, delay) {
+//   return new Promise((resolve, reject) => {
+//     const shouldResolve = Math.random() > 0.3;
+//     setTimeout(() => {
+//       if (shouldResolve) {
+//         resolve({ position, delay });
+//       } else {
+//         reject({ position, delay });
+//       }
+//     }, delay);
+//   });
+// }
+
+// const form = document.querySelector('.form');
+
+// form.addEventListener('submit', async (event) => {
+//   event.preventDefault();
+
+//   Notiflix.Loading.remove();
+
+//   const firstDelay = Number(form.querySelector('[name="delay"]').value);
+//   const delayStep = Number(form.querySelector('[name="step"]').value);
+//   const amount = Number(form.querySelector('[name="amount"]').value);
+
+//   for (let i = 1; i <= amount; i++) {
+//     const delay = firstDelay + (i - 1) * delayStep;
+//     try {
+//       const result = await createPromise(i, delay);
+//       Notiflix.Notify.success(`✅ Виконано проміс ${result.position} через ${result.delay} мс`);
+//     } catch (error) {
+//       Notiflix.Notify.failure(`❌ Відхилено проміс ${error.position} через ${error.delay} мс`);
+//     }
+//   }
+// });
+
+
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  Notiflix.Loading.remove();
+
+  const firstDelay = Number(form.querySelector('[name="delay"]').value);
+  const delayStep = Number(form.querySelector('[name="step"]').value);
+  const amount = Number(form.querySelector('[name="amount"]').value);
+
+  Notiflix.Loading.standard('Loading...');
+
+  for (let i = 1; i <= amount; i++) {
+    const delay = firstDelay + (i - 1) * delayStep;
+    try {
+      const result = await createPromise(i, delay);
+      Notiflix.Notify.success(`✅ Виконано проміс ${result.position} через ${result.delay} мс`);
+    } catch (error) {
+      Notiflix.Notify.failure(`❌ Відхилено проміс ${error.position} через ${error.delay} мс`);
+    }
+  }
+
+  Notiflix.Loading.remove();
+});
