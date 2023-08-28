2c32a612888fc396986e758f56aa18483ad1e261
Paul Derkach <mr.derkach.55@gmail.com>
2023-08-28 19:54:22 +0300

    remarks

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,47 +1,53 @@
-// import Notiflix from 'notiflix';
-
-// function createPromise(position, delay) {
-//   return new Promise((resolve, reject) => {
-//     const shouldResolve = Math.random() > 0.3;
-//     setTimeout(() => {
-//       if (shouldResolve) {
-//         resolve({ position, delay });
-//       } else {
-//         reject({ position, delay });
-//       }
-//     }, delay);
-//   });
-// }
-
-// const form = document.querySelector('.form');
-
-// form.addEventListener('submit', async (event) => {
-//   event.preventDefault();
-
-//   Notiflix.Loading.remove();
-
-//   const firstDelay = Number(form.querySelector('[name="delay"]').value);
-//   const delayStep = Number(form.querySelector('[name="step"]').value);
-//   const amount = Number(form.querySelector('[name="amount"]').value);
-
-//   for (let i = 1; i <= amount; i++) {
-//     const delay = firstDelay + (i - 1) * delayStep;
-//     try {
-//       const result = await createPromise(i, delay);
-//       Notiflix.Notify.success(`✅ Виконано проміс ${result.position} через ${result.delay} мс`);
-//     } catch (error) {
-//       Notiflix.Notify.failure(`❌ Відхилено проміс ${error.position} через ${error.delay} мс`);
-//     }
-//   }
-// });
-
-
 import Notiflix from 'notiflix';
+
+const ref = {
+  form: document.querySelector('.form'),
+  btnCreate: document.querySelector('.form button'),
+  firstDelay: document.querySelector('[name="delay"]'), // змінили ім'я поля
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+};
+
+ref.form.addEventListener('submit', submitForm);
+
+function submitForm(evt) {
+  evt.preventDefault();
+  disableElement(ref.btnCreate);
+
+  const arrayPromises = [];
+  const firstDelayValue = Number(ref.firstDelay.value);
+  const stepDelay = Number(ref.step.value);
+  const amountPromise = Number(ref.amount.value);
+  let delayPromise = firstDelayValue;
+
+  for (let i = 1; i <= amountPromise; i += 1) {
+    const promise = createPromise(i, delayPromise)
+      .then(({ position, delay }) => {
+        showNotification(
+          `✅ Fulfilled promise ${position} in ${delay}ms`,
+          'success'
+        );
+      })
+      .catch(({ position, delay }) => {
+        showNotification(
+          `❌ Rejected promise ${position} in ${delay}ms`,
+          'failure'
+        );
+      });
+
+    arrayPromises.push(promise);
+    delayPromise += stepDelay;
+  }
+
+  // Make the button active when all promises are done
+  Promise.allSettled(arrayPromises).then(() => activateElement(ref.btnCreate));
+}
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+
       if (shouldResolve) {
         resolve({ position, delay });
       } else {
@@ -51,28 +57,20 @@ function createPromise(position, delay) {
   });
 }
 
-const form = document.querySelector('.form');
-
-form.addEventListener('submit', async (event) => {
-  event.preventDefault();
-
-  Notiflix.Loading.remove();
-
-  const firstDelay = Number(form.querySelector('[name="delay"]').value);
-  const delayStep = Number(form.querySelector('[name="step"]').value);
-  const amount = Number(form.querySelector('[name="amount"]').value);
-
-  Notiflix.Loading.standard('Loading...');
-
-  for (let i = 1; i <= amount; i++) {
-    const delay = firstDelay + (i - 1) * delayStep;
-    try {
-      const result = await createPromise(i, delay);
-      Notiflix.Notify.success(`✅ Виконано проміс ${result.position} через ${result.delay} мс`);
-    } catch (error) {
-      Notiflix.Notify.failure(`❌ Відхилено проміс ${error.position} через ${error.delay} мс`);
-    }
+function showNotification(str, type) {
+  if (type === 'success') {
+    Notiflix.Notify.success(str);
+  } else if (type === 'failure') {
+    Notiflix.Notify.failure(str);
+  } else {
+    Notiflix.Notify.info(str);
   }
+}
 
-  Notiflix.Loading.remove();
-});
+function disableElement(element) {
+  element.disabled = true;
+}
+
+function activateElement(element) {
+  element.disabled = false;
+}
